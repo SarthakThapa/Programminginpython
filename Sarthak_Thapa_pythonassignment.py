32d465635b11ab562e9e3f5f14f676b46f0b1941
SarthakThapa <50993898+SarthakThapa@users.noreply.github.com>
2019-05-24 19:55:26 +0530

    Update Sarthak_Thapa_pythonassignment.py

--- Sarthak_Thapa_pythonassignment.py
+++ Sarthak_Thapa_pythonassignment.py
@@ -2,8 +2,7 @@
 
 if __name__ == '__main__':
 
-    birthdays = {
-        'Albert Einstein': '03/14/1879',
+    birthdays = {'Albert Einstein': '03/14/1879',
         'Benjamin Franklin': '01/17/1706'}
 
     print('Welcome to the birthday dictionary. We know the birthdays of:')
